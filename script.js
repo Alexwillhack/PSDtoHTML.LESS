b9cec98f17de971c18e540b48e42917b22823b27
Alexwillhack <z9110858@gmail.com>
2022-05-13 21:21:17 +0700

    magnific-popup added

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ jQuery(function($){
         arrows: false,
         dots: true
     });
+
+
+    $('.open_video').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
 });
-
-
-
