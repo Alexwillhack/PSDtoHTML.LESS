07676df23644b3d364bc1a03341f238ec42da049
Alexwillhack <z9110858@gmail.com>
2022-06-02 08:29:50 +0700

    slick-list draggable html less done

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ jQuery(function($){
            arrows: false,
            dots: true
     });
-}
+  }
     
     if($('.shop_carusel').length){
         $('.shop_carusel').slick({
@@ -17,10 +17,33 @@ jQuery(function($){
             slidesToScroll: 1,
             appendArrows: $('.shop_slider_navigation'),
             prevArrow: '<div class="left"><i class="fa-solid fa-arrow-left"></i></div>',
-            nextArrow: '<div class="right"><i class="fa-solid fa-arrow-right"></i></div>'
+            nextArrow: '<div class="right"><i class="fa-solid fa-arrow-right"></i></div>',
         });
     }
     
+    if($('.testimonial_carousel').length){
+        $('.testimonial_carousel').slick({
+            Infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            appendArrows: $('.testimonial_slider_box'),
+            prevArrow: '<div class="left_arrow"><div class="left"><i class="fa-solid fa-arrow-left"></i></div></div>',
+            nextArrow: '<div class="right_arrow"><div class="right"><i class="fa-solid fa-arrow-right"></i></div></div>',
+            responsive: [
+                {
+                  breakpoint: 480,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                   }
+                }
+            ]
+        });
+    }
+
+
+
+
     if($('.open_video').length){
         $('.open_video').magnificPopup({
           disableOn: 700,
@@ -39,7 +62,10 @@ jQuery(function($){
           mainClass: 'mfp-img-mobile',
           image: {
             verticalFit: true
-          }
+        }
         });
     }
+
 });
+
+    
